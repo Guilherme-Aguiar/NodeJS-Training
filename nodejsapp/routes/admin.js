cbc7d329b0c7371a93c0d8d0318bc7906ba3ccb0
Guilherme-Aguiar <gui9627oli@gmail.com>
2019-10-28 19:10:58 -0300

    Added dynamic template engine

--- nodejsapp/routes/admin.js
+++ nodejsapp/routes/admin.js
@@ -6,16 +6,20 @@ const rootDir = require('../helpers/path')
 
 const router = express.Router();
 
+const products = [];
+
+
 // /admin/add-product => GET
 router.get('/add-product',(req,res, next) => {
-    console.log('in another middleware!');
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
 router.post('/add-product',(req,res, next) => {
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+
+exports.products = products;
